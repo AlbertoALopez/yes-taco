0601098dfb108119bf0b9f69a4ccba0c8c7f4364
Alberto Lopez <albertolopez19@gmail.com>
2018-02-20 08:09:42 -0500

    Add redirect if no file is uploaded on results page

--- src/js/scenes/results/index.js
+++ src/js/scenes/results/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Col, Row } from 'react-flexbox-grid';
-
+import { withRouter } from 'react-router-dom';
 import Dialog from '../../components/dialog';
 import ResultsList from './resultsList';
 import Confetti from './confetti';
@@ -12,40 +12,60 @@ import './styles.scss';
 
 const isTacoInImageArray = array => array.find(label => label.description === 'taco');
 
-export const Results = props => {
-  const tacoImageObject = isTacoInImageArray(props.imageLabels) || null;
+export class Results extends Component {
+  static propTypes = {
+    imageLabels: PropTypes.array,
+    file: PropTypes.object.isRequired,
+    history: PropTypes.shape({
+      push: PropTypes.func.isRequired,
+    }).isRequired,
+  }
 
-  return (
-    <div className="results-container">
-      <Col xs={12}>
-        <Row center="xs">
-          <Dialog>
-              {
-                tacoImageObject !== null ?
-                  <div>
-                    <h3>Yes Taco!</h3>
-                    <Confetti />
-                  </div>
-                :
-                  <div className="match-container">
-                    <h3>No tacos found. These were other matches for your image</h3>
-                    <ResultsList imageLabels={props.imageLabels} />
-                  </div>
-              }
-          </Dialog>
-        </Row>
-      </Col>
-    </div>
-  );
-};
+  constructor(props) {
+    super(props);
+  }
 
-Results.propTypes = {
-  imageLabels: PropTypes.array,
-};
+  componentDidMount() {
+    // If no file has been uploaded
+    if (!this.props.file) {
+      this.props.history.push('/upload');
+    }
+  }
+  render() {
+    const tacoImageObject = isTacoInImageArray(this.props.imageLabels) || null;
+
+    return (
+      <div className="results-container">
+        <Col xs={12}>
+          <Row center="xs">
+            <Dialog>
+                {
+                  tacoImageObject !== null ?
+                    <div>
+                      <h3>Yes Taco!</h3>
+                      <Confetti />
+                    </div>
+                  :
+                    <div className="match-container">
+                      <h3>No tacos found. These were other matches for your image</h3>
+                      <ResultsList imageLabels={this.props.imageLabels} />
+                    </div>
+                }
+            </Dialog>
+          </Row>
+        </Col>
+      </div>
+    );
+  }
+}
 
 
 const mapStateToProps = state => ({
   imageLabels: state.cloudDetection.detectedImageLabels,
+  file: state.imageUpload.file,
 });
 
-export default connect(mapStateToProps, null)(Results);
+// Create HOC for react router
+const ResultsWithRouter = ({ history }) => (Results.render);
+
+export default connect(mapStateToProps, null)(withRouter(Results));
--- src/js/services/uploadImage/actions.js
+++ src/js/services/uploadImage/actions.js
@@ -10,6 +10,15 @@ const app = firebase.initializeApp(config);
 // Create reference to image bucket
 const storageRef = app.storage().ref('images');
 
+export const SET_FILE_BLOB = 'SET_FILE_BLOB';
+
+const setFileBlob = file => {
+  return {
+    type: SET_FILE_BLOB,
+    file,
+  };
+};
+
 export const UPLOAD_IMAGE_START = 'UPLOAD_IMAGE_START';
 
 const uploadImageStart = file => {
@@ -45,6 +54,7 @@ export const UPLOAD_IMAGE = 'UPLOAD_IMAGE';
 
 const uploadImage = file => {
   return function action(dispatch) {
+    dispatch(setFileBlob(file));
     dispatch(uploadImageStart(file));
 
     // Create a child refrence from the images folder
